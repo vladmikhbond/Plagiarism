babdd2b291c7a46fd8c88f4f291e67ec9773d9db
volodymyr bondariev <vladmikhbond@hotmail.com>
2015-09-02 10:02:26 +0300

    init commit

---
+++ Plagiarism/XComparer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plagiarism
+{
+    public class XComparer
+    {
+        public class ReportItem
+        {
+            public string BookName;
+            public int Start;
+            public int Length;
+        }
+        
+        public int N { set; get; }
+        public string  Manuscript { set; get; }
+
+        public XComparer(string m, int n)
+        {
+            Manuscript = m;
+            N = n;
+        }
+
+        public List<ReportItem> Compare(string bookName, string book)
+        {
+            var result = new List<ReportItem>();
+            int pos = 0;
+            // отбираем пробы в манускрипте с шагом N и длиной N/2
+            while (pos < Manuscript.Length - N)
+            {
+                // ищем расширенную пробу в книге 
+                var item = IndexOfMax(book, pos);
+                
+                if (item != null)
+                {
+                    item.BookName = bookName;
+                    result.Add(item);
+                    pos += item.Length; 
+                }
+                pos += N;
+            }
+            return result;
+        }
+
+        // Ищет вхождения части манускрипта в книгу. Часть начинается с позиции pos и имеен длину N символов.
+        // Находит только первое вхождение, хотя должен искать максимальное из всех.
+        //
+        public ReportItem IndexOfMax(string book, int pos) {
+            string sample = Manuscript.Substring(pos, N);
+            int ib = Find (book, sample, 0);
+            // если проба нашлась, расширяем ее до максимума
+            if (ib > -1)
+            {
+                int im = ExtStart(book, ib, pos);
+                int jm = ExtFinish(book, ib + N, pos + N);
+                return new ReportItem { Start = im, Length = jm - im };
+            }
+            return null;
+        }
+
+        // Сдвигаем начало 
+        //
+        public int ExtStart(string book, int ib, int im)
+        {
+            while (im >= 0 && ib >= 0 && book[ib] == Manuscript[im])
+            {
+                ib--;
+                im--;
+            };
+            return im + 1;
+        }
+
+        // Сдвигаем конец 
+        //
+        public int ExtFinish(string book, int jb, int jm)
+        {
+            while (jb < book.Length && jm < Manuscript.Length && book[jb] == Manuscript[jm])
+            {
+                jb++;
+                jm++;
+            }
+            return jm;
+        }
+
+
+        // Поиск образца в тексте
+        //
+        public static int Find(string text, string sample, int startIndex)
+        {
+            return text.IndexOf(sample, startIndex);
+        }
+    }
+}
---
+++ UnitTestPlagiarism/UnitTestXComparer.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Plagiarism;
+using System.IO;
+
+namespace UnitTestPlagiarism
+{
+    [TestClass]
+    public class UnitTestXComparer
+    {
+
+        [TestMethod]
+        public void Find_1()
+        {
+            string book = File.ReadAllText(@"Resources\Text1.txt");
+            var sample = "Шаблон проектирования программы в виде трех слабо связанных между собой частей: модели, представления и контроллера известен в программировании давно";
+            int res = XComparer.Find(book, sample, 0);
+            Assert.AreEqual(259140, res);
+        }
+
+
+        [TestMethod]
+        public void Find_2()
+        {
+            int L = 10000, D = 1000;
+            string book = File.ReadAllText(@"Resources\Text1.txt");
+            var sample = book.Substring(L, D);
+            int res = XComparer.Find(book, sample, 0);
+            Assert.AreEqual(L, res);
+        }
+
+
+        [TestMethod]
+        public void ExtStart_1()
+        {
+            var book = "01234567890123456789";
+            var manu = "00034567890123400";
+            var xc = new XComparer(manu, 5);
+            int res = xc.ExtStart(book, 5, 5);
+            Assert.AreEqual(3, res);
+        }
+
+        [TestMethod]
+        public void ExtStart_2()
+        {
+            var book = "01234567890123456789";
+            var manu = "0001234567890123400";
+            var xc = new XComparer(manu, 5);
+            int res = xc.ExtStart(book, 5, 7);
+            Assert.AreEqual(2, res);
+        }
+
+        [TestMethod]
+        public void ExtStart_3()
+        {
+            var book = "0001234567890123456789";
+            var manu = "01234567890123400";
+            var xc = new XComparer(manu, 5);
+            int res = xc.ExtStart(book, 7, 5);
+            Assert.AreEqual(0, res);
+        }
+
+        [TestMethod]
+        public void ExtFinish_1()
+        {
+            var book = "01234567890123456789";
+            var manu = "00034567890123400";
+            var xc = new XComparer(manu, 5);
+            int res = xc.ExtFinish(book, 10, 10);
+            Assert.AreEqual(15, res);
+        }
+
+        [TestMethod]
+        public void ExtFinish_2()
+        {
+            var book = "01234567890123456789";
+            var manu = "0000034567890123400";
+            var xc = new XComparer(manu, 5);
+            int res = xc.ExtFinish(book, 10, 12);
+            Assert.AreEqual(17, res);
+        }
+
+        [TestMethod]
+        public void ExtFinish_3()
+        {
+            var book = "0001234567890123456789";
+            var manu = "00034567890123400";
+            var xc = new XComparer(manu, 5);
+            int res = xc.ExtFinish(book, 12, 10);
+            Assert.AreEqual(15, res);
+        }
+
+        [TestMethod]
+        public void Compare_1()
+        {
+            var book = "01234567890123456789";
+            var manu = "00034567890123400";
+            var xc = new XComparer(manu, 5);
+            var res = xc.Compare("", book);
+
+            Assert.AreEqual(3, res[0].Start);
+            Assert.AreEqual(12, res[0].Length);
+        }
+
+        [TestMethod]
+        public void Compare_2()
+        {
+            var book = "01234567890123456789";
+            var manu = "0000034567890123400";
+            var xc = new XComparer(manu, 5);
+            var res = xc.Compare("", book);
+
+            Assert.AreEqual(5, res[0].Start);
+            Assert.AreEqual(12, res[0].Length);
+        }
+
+        [TestMethod]
+        public void Compare_3()
+        {
+            var book = "0001234567890123456789";
+            var manu = "00034567890123400";
+            var xc = new XComparer(manu, 5);
+            var res = xc.Compare("", book);
+
+            Assert.AreEqual(3, res[0].Start);
+            Assert.AreEqual(12, res[0].Length);
+        }
+
+        [TestMethod]
+        public void Compare_4()
+        {
+            int L = 10000, D = 1000;
+            string book = File.ReadAllText(@"Resources\Text1.txt");
+            var manu = new string('0', L) + book.Substring(L, D) + new string('0', L);
+            var xc = new XComparer(manu, 100);
+            var res = xc.Compare("", book);
+
+            Assert.AreEqual(L, res[0].Start);
+            Assert.AreEqual(D, res[0].Length);
+        }
+
+
+
+
+
+
+
+    }
+}
